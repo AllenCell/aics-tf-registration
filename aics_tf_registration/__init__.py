a9eaafec98afe29c0eeff3206969869015f26c0c
Jianxu Chen <jianxuc@alleninstitute.org>
2020-11-22 20:18:25 -0800

    Bump version: 0.1.0 → 0.1.1

--- aics_tf_registration/__init__.py
+++ aics_tf_registration/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Mark Filip Sluzewski"
 __email__ = "filip.sluzewski@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def get_module_version():
